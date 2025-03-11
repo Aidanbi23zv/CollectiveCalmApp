0ad35a749a5e477af2861b093ec898ca17e477fb
Aidan Dunsmore <bi23zv@student.sunderland.ac.uk>
2025-03-11 15:42:50 +0000

    Added info to home pafe

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,8 +7,14 @@ function Home() {
         <div className="login">
             <h2>LOGIN</h2>
         </div>
+        <div className="content">
         <div className="logo">
           <img src="/logo_ph.jpg" alt="logo" />
+        </div>
+        <div className="info">
+          <p>Mental health is often overlooked in the modern day but we are very likely to know someone with a mental health issue, whether it's visible or not. We aim to mold a world that is happier and healthier. 
+            Tagline: Making life easier for everyone. Together. </p>
+        </div>
         </div>
       </div>
     );
